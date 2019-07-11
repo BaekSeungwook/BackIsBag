67499b5351227093296fc8ad6e7223d20befda3c
kosta <kosta@192.168.0.104>
2019-07-11 19:22:32 +0900

    검색기능 추가 및 views수정

---
+++ src/kosta/product/controller/ProductBySearchController.java
@@ -0,0 +1,40 @@
+package kosta.product.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+import kosta.bag.Controller;
+import kosta.bag.ModelAndView;
+import kosta.product.model.dto.ProductDTO;
+import kosta.product.model.service.ProductService;
+
+public class ProductBySearchController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("ProductBySearchController의 handleRequest호출");
+		String url = "views/errorView/error.jsp";
+		try {
+			String keyField = request.getParameter("keyField");
+			String keyWord = request.getParameter("keyWord");
+			
+			List<ProductDTO> list = ProductService.selectBySearch(keyField, keyWord);
+			request.setAttribute("list", list);
+			url="views/product/list.jsp";
+			//url="index.jsp";
+		} catch (SQLException e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", e.getMessage());
+		}
+		ModelAndView mv = new ModelAndView();
+		mv.setPath(url);//isRedirect()의 리턴값 false.(forward로보내므로)//redirect로보낼땐 mv를 true로
+		return mv;
+	}
+}
